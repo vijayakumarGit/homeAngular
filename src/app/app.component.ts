dc38e5cafe1421c8f654835ee57713e40953fba6
Vijaykumar <Vijayakumar@hakunamatata.in>
2017-03-02 16:53:36 +0530

    Directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles:[`div{width:300px;h                                                                                                                                                                                                    eight:200px}`]
+
+
 })
 export class AppComponent {
   title = 'app works!';
+  delete:Boolean=false;
+  test:string='Starting value';
+  boundValue:number=300;
+  touchInput(value:string){
+    console.log(value)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {SampleCOmponet} from './sampleComponent/SampleComponet';
+import {SampleTwoComponent} from './sampleComponent/SampleTwoComponent';
+import {LifeCycleHook} from './sampleComponent/lfecycleHook';
+import {CustomDirective} from './Directive/Custom.derective'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SampleCOmponet,
+    SampleTwoComponent,
+    LifeCycleHook,
+    CustomDirective
   ],
   imports: [
     BrowserModule,
